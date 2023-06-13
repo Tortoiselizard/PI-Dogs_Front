b4e2065f3bc5ad76e94a5307c81a8b90e59186de
Troy Pernía <troy00pernia@gmail.com>
2023-06-13 17:17:45 -0400

    change components/MainPage: delete functions and constants that were not working

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import {updateTemperaments, getAllTemperaments, getAllDogs, keepDogs, getAllDogs2} from "../../redux/actions/index"
+import { getAllTemperaments, getAllDogs2} from "../../redux/actions/index"
 import {useDispatch} from "react-redux"
 import styles from "./MainPage.module.css"
 
@@ -8,22 +8,10 @@ function MainPage() {
 
     const dispatch = useDispatch()
 
-    // React.useEffect(async () => {
-    //     await dispatch(updateTemperaments())
-    //     await dispatch(getAllTemperaments())
-    //     const dogs = await getAllDogs()
-    //     if (typeof(dogs.payload)==="string") {alert(dogs.payload)}
-    //     else {
-    //         await dispatch(dogs)
-    //         await dispatch(keepDogs(dogs.payload))
-    //     }
-    // }, [dispatch])
-
     React.useEffect(() => {
-        dispatch(updateTemperaments())
         dispatch(getAllTemperaments())
         dispatch(getAllDogs2())
-    }, [])
+    }, [dispatch])
 
     return <div className={styles.MainPage}>
         <h1>Henry Dogs</h1>        
