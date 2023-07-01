378c63db156c65fd9453bb28dc75e6674a3fc254
Troy Pernía <troy00pernia@gmail.com>
2023-07-01 14:22:15 -0400

    change components/DogCard: change component render

--- src/components/DogCard/DogCard.jsx
+++ src/components/DogCard/DogCard.jsx
@@ -1,25 +1,11 @@
-// import { useEffect, useRef } from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import style from './DogCard.module.css'
 
 const DogCard = (props) => {
-  // return (
-  //   <div className={style.DogCard}>
-  //     <div>
-  //       <Link to={`/dog/${props.name}`}><label className={style.name}>{props.name}</label></Link>
-  //     </div>
-  //     <img src={props.image} alt={props.name} className={style.imagen} />
-  //     <div className={style.textoDogCard}>
-  //       <p className={style.parrafos}><span className={style.encabezadoTexto}>Temperament:</span> <span>{props.temperament}</span></p>
-  //       <p className={style.parrafos}><span className={style.encabezadoTexto}>Weight (Lb):</span> <span>{props.weight}</span></p>
-  //     </div>
-  //   </div>
-  // )
-
   return (
     <div className={style.DogCardContainer} style={{ backgroundImage: `url(${props.image})` }}>
       <div className={style.OverviewContainer}>
-        <h2>{props.name}</h2>
+        <Link to={`/dog/${props.name}`} className={style.Link}><h2 id={style.title}>{props.name}</h2></Link>
         <div>
           <h3>Temperament</h3>
           <p>{props.temperament}</p>
