fc4c13cc4ce84a72d9114d20f9c913447febeb05
Troy Pernía <troy00pernia@gmail.com>
2023-07-01 12:51:53 -0400

    change components/MainPage: removed useEffect and component Loading (line 4-7, 17)

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,32 +1,19 @@
-import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-
-import { useDispatch } from 'react-redux'
-
-import Loading from '../Loading/Loading'
-
-import { getAllTemperaments, getAllDogs2 } from '../../redux/actions/index'
 import styles from './MainPage.module.css'
 
-function MainPage () {
-  const dispatch = useDispatch()
-  const [loading, setLoading] = useState(false)
-
-  useEffect(() => {
-    dispatch(getAllTemperaments())
-    dispatch(getAllDogs2(null, setLoading))
-  }, [dispatch])
+function MainPage ({ sliding }) {
+  function playSlide () {
+    sliding('/', '/home')
+  }
 
   return (
     <>
-      <div className={styles.MainPage}>
-        <div>
+      <div id='MainPageSlide' className={styles.MainPage}>
+        <div className={styles.verticalBar}>
           <h1>Find your new friend</h1>
-          <Link to='/home'><button className={styles.buttonMainPage}>Search</button></Link>
+          <Link to='/home'><button onClick={playSlide} className={styles.buttonMainPage}>Search</button></Link>
         </div>
-
       </div>
-      <Loading loading={loading} />
     </>
   )
 }
