8f0a6fa490a269c15803bfaebc6d8eb6856aae28
Troy Pernía <troy00pernia@gmail.com>
2023-07-12 17:44:15 -0400

    add controllers/message.js

---
+++ src/controllers/message.js
@@ -0,0 +1,22 @@
+module.exports = {
+  EXPLORE: {
+    title: 'EXPLORE',
+    imageContent: '',
+    contentMessage: 'Este es el mensaje de descripción de la aplicación'
+  },
+  API: {
+    title: 'API',
+    imageContent: '',
+    contentMessage: 'Este es el mensaje para decir que api estas usando'
+  },
+  ABOUT: {
+    title: 'ABOUT ME',
+    imageContent: '',
+    contentMessage: 'Este es el mensaje para hablar de ti'
+  },
+  CONTACT: {
+    title: 'CONTACT',
+    imageContent: '',
+    contentMessage: 'Este es el mensaje para decir tu gmail, github, linkedIn, etc.'
+  }
+}
