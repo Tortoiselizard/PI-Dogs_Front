9f413c53c72ca3ae9ac6a1498e131c7addc8a8e4
Troy Pernía <troy00pernia@gmail.com>
2023-07-15 14:28:38 -0400

    change components/Nav: add useEffect (line 11-20)

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 // import './nav.css';
-import React from 'react'
+import React, { useEffect } from 'react'
 import SearchDog from '../SearchDog/SearchDog.jsx'
 import { NavLink, useLocation } from 'react-router-dom'
 
@@ -8,17 +8,27 @@ import style from './Nav.module.css'
 
 function Nav ({ setLoad }) {
   const location = useLocation()
+  useEffect(() => {
+    if (location.pathname === '/home') {
+      const div = document.getElementsByName('toHome')[0]
+      div.classList.toggle(style.butonActiveHome)
+    }
+    if (location.pathname === '/dog/create') {
+      const div = document.getElementsByName('toCreate')[0]
+      div.classList.toggle(style.butonActiveCreate)
+    }
+  }, [location.pathname])
 
   return (
     <div className={style.Nav}>
       <div className={style.links}>
         <NavLink to='/home'>
-          <div className={style.containerButton}>
+          <div name='toHome' className={style.containerButton}>
             <button className={style.buttonHome}><img src='https://cdn-icons-png.flaticon.com/512/25/25694.png' alt='homeLogo' /></button>
           </div>
         </NavLink>
         <NavLink to='/dog/create'>
-          <div className={style.containerButton}>
+          <div name='toCreate' className={style.containerButton}>
             <button className={style.buttonDog}><img src='https://aux.iconspalace.com/uploads/dog-paw-icon-256.png' alt='CreateLogo' /></button>
           </div>
         </NavLink>
