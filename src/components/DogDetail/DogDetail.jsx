6bc1a04918aa830728e63c788eeef4b27b14e7cb
Troy Pernía <troy00pernia@gmail.com>
2023-07-16 17:36:15 -0400

    change components/DogDetail: add condition to add lifeSpan (line 54, 61-71)

--- src/components/DogDetail/DogDetail.jsx
+++ src/components/DogDetail/DogDetail.jsx
@@ -51,14 +51,24 @@ function DogDetail ({ store }) {
             name: store.dogDetail[0].name,
             height: { min: store.dogDetail[0].height.split('-')[0].trim(), max: store.dogDetail[0].height.split('-')[1].trim() },
             weight: { min: store.dogDetail[0].weight.split('-')[0].trim(), max: store.dogDetail[0].weight.split('-')[1].trim() },
-            lifeSpan: store.dogDetail[0].lifeSpan
+            lifeSpan: store.dogDetail[0].lifeSpan && store.dogDetail[0].lifeSpan.includes('-')
               ? {
                   min: store.dogDetail[0].lifeSpan.split('-')[0].trim(),
                   max: !store.dogDetail[0].lifeSpan.split('-')[1].includes('y')
                     ? store.dogDetail[0].lifeSpan.split('-')[1].trim()
                     : store.dogDetail[0].lifeSpan.split('-')[1].split('y')[0].trim()
                 }
-              : { min: '', max: '' },
+              : store.dogDetail[0].lifeSpan && store.dogDetail[0].lifeSpan.includes('–')
+                ? {
+                    min: store.dogDetail[0].lifeSpan.split('–')[0].trim(),
+                    max: !store.dogDetail[0].lifeSpan.split('–')[1].includes('y')
+                      ? store.dogDetail[0].lifeSpan.split('–')[1].trim()
+                      : store.dogDetail[0].lifeSpan.split('–')[1].split('y')[0].trim()
+                  }
+                : {
+                    min: '',
+                    max: ''
+                  },
             image: store.dogDetail[0].image,
             temperament: store.temperaments.filter(t => dataTemperaments.includes(t.name))
           }
